2b52c7af31935eaa57a7582bf3cd0aee4021346f
Beck <chimyon72@gmail.com>
2024-05-22 19:57:11 -0700

    added testing to find time difference to execute my findpermutationdifference

--- HashTables/3146. Find Permutation Difference.py
+++ HashTables/3146. Find Permutation Difference.py
@@ -1,23 +1,42 @@
 class Solution:  
     # this brute force solution has the time complexity O(n**2)  
-    # def findPermutationDifference(self, s: str, t: str) -> int:
-    #     permutation_difference = 0
-    #     for i, letter1 in enumerate(s):
-    #         for j, letter2 in enumerate(t):
-    #             if letter1 == letter2:
-    #                 permutation_difference += abs(i - j)
+    def findPermutationDifference_original(self, s: str, t: str) -> int:
+        permutation_difference = 0
+        for i, letter1 in enumerate(s):
+            for j, letter2 in enumerate(t):
+                if letter1 == letter2:
+                    permutation_difference += abs(i - j)
         
-    #     return permutation_difference
+        return permutation_difference
     
     # Using Index map to save indecies of characters have instant look up O(1)
-    def findPermutationDifference(self, s:str, t:str) -> int:
+    def findPermutationDifference_optimized(self, s:str, t:str) -> int:
         idx_map = {char:idx for idx, char in enumerate(t)}
 
         #Calculation permutation difference
         return sum(abs(i - idx_map[char]) for i, char in enumerate(s))
 
-test1 = Solution()
-test1.findPermutationDifference("abc", "bas")
-print(test1.findPermutationDifference("abc", "bas"))
+#Adding Testing to find time difference
+import timeit
+def test_findPermutationDifference():
+    sol = Solution()
 
+    # Test cases
+    test_cases = [
+        ("abc", "bac"),
+        ("abcde", "edbac"),
+        ("abcdefghijklmnopqrstuvwxyz", "zyxwvutsrqponmlkjihgfedcba")
+    ]
 
+    for s, t in test_cases:
+        # Timing the original solution
+        original_time = timeit.timeit(lambda: sol.findPermutationDifference_original(s, t), number=1000)
+        # Timing the optimized solution
+        optimized_time = timeit.timeit(lambda: sol.findPermutationDifference_optimized(s, t), number=1000)
+
+        print(f"Test case s='{s}' t='{t}'")
+        print(f"Original time: {original_time:.6f} seconds")
+        print(f"Optimized time: {optimized_time:.6f} seconds")
+        print()
+
+test_findPermutationDifference()
